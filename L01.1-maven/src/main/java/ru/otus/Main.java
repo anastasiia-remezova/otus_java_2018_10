8215723cdea0536419ef57379edf239ccb67d9f2
anastasia <aaremez@mail.ru>
2018-11-13 00:09:23 +0300

    maven

---
+++ L01.1-maven/src/main/java/ru/otus/Main.java
@@ -0,0 +1,24 @@
+package ru.otus;
+
+
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.Multimap;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Multimap<String, String> map;
+        String lastName = "Peppi";
+        String middleName = null;
+        String firstName = "LongStocking";
+        int zipCode = 123456;
+        System.out.println(MoreObjects.toStringHelper(Object.class)
+                .omitNullValues()
+                .add("lastName", lastName)
+                .add("middleName", middleName)
+                .add("firstName", firstName)
+                .add("zipCode", zipCode)
+                .toString());
+    }
+
+}
